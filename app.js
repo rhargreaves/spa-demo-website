ebcc8044040a2c8da1b8eafea681bc32fea72c43
rhargreaves <robert.hargreaves@7digital.com>
2016-09-01 15:07:41 +0100

    Update clouds vid URL

--- app.js
+++ app.js
@@ -10,27 +10,27 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    res.render('home');
+	res.render('home');
 });
 
 app.get('/slow/clouds.mp4', function (req, res) {
 	setTimeout(function() {
-    var options = {
-      url: 'https://s3-eu-west-1.amazonaws.com/robh-spa-2016-assets/clouds.mp4',
-      headers: {
-        range: req.headers.range
-      }
-    };
-    request(options).pipe(res);
+		var options = {
+			url: 'https://s3-eu-west-1.amazonaws.com/robh-tdd-cdn-demo-site-resources/clouds.mp4',
+			headers: {
+				range: req.headers.range
+			}
+		};
+		request(options).pipe(res);
 	}, 2000);
 });
 
 app.get('/counter', function(req, res) {
-  res.send({ value: counter });
+	res.send({ value: counter });
 });
 
 setInterval(function(){
-  counter++;
+	counter++;
 }, 333);
 
 
